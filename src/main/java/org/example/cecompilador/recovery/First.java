24cdae503dfd23b37159c4db55283f11b937227f
Heitor Aparecido <heitor.ap.silva98@gmail.com>
2025-07-01 11:59:08 -0300

    Atualizacoes no retorno de erros

--- src/main/java/org/example/cecompilador/recovery/First.java
+++ src/main/java/org/example/cecompilador/recovery/First.java
@@ -2,19 +2,28 @@ package org.example.cecompilador.recovery;
 import org.example.cecompilador.compilador.CeCompilador;
 
 public class First {
-	
+
 	static public final RecoverySet main = new RecoverySet();
 	static public final RecoverySet declaraVariavel = new RecoverySet();
-	static public final RecoverySet tipo = new RecoverySet();
+	static public final RecoverySet declaraTipo = new RecoverySet();
 	static public final RecoverySet atribuicao = new RecoverySet();
 	static public final RecoverySet estruturaRepeticao = new RecoverySet();
+	static public final RecoverySet estruturaWhile = new RecoverySet();
+	static public final RecoverySet estruturaDoWhile = new RecoverySet();
 	static public final RecoverySet fator = new RecoverySet();
 	static public final RecoverySet termo = new RecoverySet();
+	static public final RecoverySet expressao = new RecoverySet();
+	static public final RecoverySet expressaoRelacional = new RecoverySet();
 	static public final RecoverySet expressaoAritmetica = new RecoverySet();
-	
+
+	static public final RecoverySet instrucao = new RecoverySet();
+	static public final RecoverySet blocoInstrucao = new RecoverySet();
+	static public final RecoverySet estruturaCondicional = new RecoverySet();
+
 	static {
-		
+
 		main.add(new Integer(CeCompilador.EOF));
+		/*
 		declaraVariavel.add(new Integer(CeCompilador.FIMINSTRUCAO));
 		declaraVariavel.add(new Integer(CeCompilador.TIPOINT));
 		declaraVariavel.add(new Integer(CeCompilador.TIPOFLOAT));
@@ -26,8 +35,146 @@ public class First {
 		fator.add(new Integer(CeCompilador.CONST_INT));
 		fator.add(new Integer(CeCompilador.CONST_FLOAT));
 		fator.add(new Integer(CeCompilador.ABREPAR));
+		*/
+
+		/////////////////////////////////////////////////
+
+		instrucao.add(new Integer(CeCompilador.TIPOINT));
+		instrucao.add(new Integer(CeCompilador.TIPOFLOAT));
+		instrucao.add(new Integer(CeCompilador.TIPOBOL));
+		instrucao.add(new Integer(CeCompilador.ID));
+		instrucao.add(new Integer(CeCompilador.CONST_INT));
+		instrucao.add(new Integer(CeCompilador.CONST_FLOAT));
+		instrucao.add(new Integer(CeCompilador.ABREPAR));
+		instrucao.add(new Integer(CeCompilador.ABREBLOCO));
+		instrucao.add(new Integer(CeCompilador.IF));
+		instrucao.add(new Integer(CeCompilador.WHILE));
+		instrucao.add(new Integer(CeCompilador.DO));
+		instrucao.add(new Integer(CeCompilador.FECHABLOCO));
+
+		blocoInstrucao.add(new Integer(CeCompilador.TIPOINT));
+		blocoInstrucao.add(new Integer(CeCompilador.TIPOFLOAT));
+		blocoInstrucao.add(new Integer(CeCompilador.TIPOBOL));
+		blocoInstrucao.add(new Integer(CeCompilador.ID));
+		blocoInstrucao.add(new Integer(CeCompilador.CONST_INT));
+		blocoInstrucao.add(new Integer(CeCompilador.CONST_FLOAT));
+		blocoInstrucao.add(new Integer(CeCompilador.ABREPAR));
+		blocoInstrucao.add(new Integer(CeCompilador.ABREBLOCO));
+		blocoInstrucao.add(new Integer(CeCompilador.IF));
+		blocoInstrucao.add(new Integer(CeCompilador.WHILE));
+		blocoInstrucao.add(new Integer(CeCompilador.DO));
+		blocoInstrucao.add(new Integer(CeCompilador.FECHABLOCO));
+		blocoInstrucao.add(new Integer(CeCompilador.DOWHILE));
+
+		declaraVariavel.add(new Integer(CeCompilador.TIPOINT));
+		declaraVariavel.add(new Integer(CeCompilador.TIPOFLOAT));
+		declaraVariavel.add(new Integer(CeCompilador.TIPOBOL));
+		declaraVariavel.add(new Integer(CeCompilador.ID));
+		declaraVariavel.add(new Integer(CeCompilador.CONST_INT));
+		declaraVariavel.add(new Integer(CeCompilador.CONST_FLOAT));
+		declaraVariavel.add(new Integer(CeCompilador.ABREPAR));
+		declaraVariavel.add(new Integer(CeCompilador.ABREBLOCO));
+		declaraVariavel.add(new Integer(CeCompilador.IF));
+		declaraVariavel.add(new Integer(CeCompilador.WHILE));
+		declaraVariavel.add(new Integer(CeCompilador.DO));
+		declaraVariavel.add(new Integer(CeCompilador.FECHABLOCO));
+
+		//declaraTipo.add(new Integer(CeCompilador.ID));
+
+		atribuicao.add(new Integer(CeCompilador.TIPOINT));
+		atribuicao.add(new Integer(CeCompilador.TIPOFLOAT));
+		atribuicao.add(new Integer(CeCompilador.TIPOBOL));
+		atribuicao.add(new Integer(CeCompilador.ID));
+		atribuicao.add(new Integer(CeCompilador.CONST_INT));
+		atribuicao.add(new Integer(CeCompilador.CONST_FLOAT));
+		atribuicao.add(new Integer(CeCompilador.ABREPAR));
+		atribuicao.add(new Integer(CeCompilador.ABREBLOCO));
+		atribuicao.add(new Integer(CeCompilador.IF));
+		atribuicao.add(new Integer(CeCompilador.WHILE));
+		atribuicao.add(new Integer(CeCompilador.DO));
+		atribuicao.add(new Integer(CeCompilador.FECHABLOCO));
+
+		declaraTipo.add(new Integer(CeCompilador.ID));
+
+		estruturaCondicional.add(new Integer(CeCompilador.TIPOINT));
+		estruturaCondicional.add(new Integer(CeCompilador.TIPOFLOAT));
+		estruturaCondicional.add(new Integer(CeCompilador.TIPOBOL));
+		estruturaCondicional.add(new Integer(CeCompilador.ID));
+		estruturaCondicional.add(new Integer(CeCompilador.CONST_INT));
+		estruturaCondicional.add(new Integer(CeCompilador.CONST_FLOAT));
+		estruturaCondicional.add(new Integer(CeCompilador.ABREPAR));
+		estruturaCondicional.add(new Integer(CeCompilador.ABREBLOCO));
+		estruturaCondicional.add(new Integer(CeCompilador.IF));
+		estruturaCondicional.add(new Integer(CeCompilador.WHILE));
+		estruturaCondicional.add(new Integer(CeCompilador.DO));
+		estruturaCondicional.add(new Integer(CeCompilador.FECHABLOCO));
+
+		estruturaRepeticao.add(new Integer(CeCompilador.TIPOINT));
+		estruturaRepeticao.add(new Integer(CeCompilador.TIPOFLOAT));
+		estruturaRepeticao.add(new Integer(CeCompilador.TIPOBOL));
+		estruturaRepeticao.add(new Integer(CeCompilador.ID));
+		estruturaRepeticao.add(new Integer(CeCompilador.CONST_INT));
+		estruturaRepeticao.add(new Integer(CeCompilador.CONST_FLOAT));
+		estruturaRepeticao.add(new Integer(CeCompilador.ABREPAR));
+		estruturaRepeticao.add(new Integer(CeCompilador.ABREBLOCO));
+		estruturaRepeticao.add(new Integer(CeCompilador.IF));
+		estruturaRepeticao.add(new Integer(CeCompilador.WHILE));
+		estruturaRepeticao.add(new Integer(CeCompilador.DO));
+		estruturaRepeticao.add(new Integer(CeCompilador.FECHABLOCO));
+
+		estruturaWhile.add(new Integer(CeCompilador.TIPOINT));
+		estruturaWhile.add(new Integer(CeCompilador.TIPOFLOAT));
+		estruturaWhile.add(new Integer(CeCompilador.TIPOBOL));
+		estruturaWhile.add(new Integer(CeCompilador.ID));
+		estruturaWhile.add(new Integer(CeCompilador.CONST_INT));
+		estruturaWhile.add(new Integer(CeCompilador.CONST_FLOAT));
+		estruturaWhile.add(new Integer(CeCompilador.ABREPAR));
+		estruturaWhile.add(new Integer(CeCompilador.ABREBLOCO));
+		estruturaWhile.add(new Integer(CeCompilador.IF));
+		estruturaWhile.add(new Integer(CeCompilador.WHILE));
+		estruturaWhile.add(new Integer(CeCompilador.DO));
+		estruturaWhile.add(new Integer(CeCompilador.FECHABLOCO));
+
+		estruturaDoWhile.add(new Integer(CeCompilador.TIPOINT));
+		estruturaDoWhile.add(new Integer(CeCompilador.TIPOFLOAT));
+		estruturaDoWhile.add(new Integer(CeCompilador.TIPOBOL));
+		estruturaDoWhile.add(new Integer(CeCompilador.ID));
+		estruturaDoWhile.add(new Integer(CeCompilador.CONST_INT));
+		estruturaDoWhile.add(new Integer(CeCompilador.CONST_FLOAT));
+		estruturaDoWhile.add(new Integer(CeCompilador.ABREPAR));
+		estruturaDoWhile.add(new Integer(CeCompilador.ABREBLOCO));
+		estruturaDoWhile.add(new Integer(CeCompilador.IF));
+		estruturaDoWhile.add(new Integer(CeCompilador.WHILE));
+		estruturaDoWhile.add(new Integer(CeCompilador.DO));
+		estruturaDoWhile.add(new Integer(CeCompilador.FECHABLOCO));
+
+		expressao.add(new Integer(CeCompilador.FIMINSTRUCAO));
+		expressao.add(new Integer(CeCompilador.FECHAPAR));
+
+		expressaoRelacional.add(new Integer(CeCompilador.FIMINSTRUCAO));
+		expressaoRelacional.add(new Integer(CeCompilador.FECHAPAR));
+		expressaoRelacional.add(new Integer(CeCompilador.OPBOOL));
+
+		expressaoAritmetica.add(new Integer(CeCompilador.FIMINSTRUCAO));
+		expressaoAritmetica.add(new Integer(CeCompilador.FECHAPAR));
+		expressaoAritmetica.add(new Integer(CeCompilador.OPBOOL));
+		expressaoAritmetica.add(new Integer(CeCompilador.OPREL));
+
+		termo.add(new Integer(CeCompilador.FIMINSTRUCAO));
+		termo.add(new Integer(CeCompilador.FECHAPAR));
+		termo.add(new Integer(CeCompilador.OPBOOL));
+		termo.add(new Integer(CeCompilador.OPREL));
+		termo.add(new Integer(CeCompilador.OPAD));
+
+		fator.add(new Integer(CeCompilador.FIMINSTRUCAO));
+		fator.add(new Integer(CeCompilador.FECHAPAR));
+		fator.add(new Integer(CeCompilador.OPBOOL));
+		fator.add(new Integer(CeCompilador.OPREL));
+		fator.add(new Integer(CeCompilador.OPAD));
+		fator.add(new Integer(CeCompilador.OPMULT));
+
 		/*
-		termo.add(new Integer(CeCompilador.ID));      
+		termo.add(new Integer(CeCompilador.ID));
 		termo.add(new Integer(CeCompilador.CONST_INT));
 		termo.add(new Integer(CeCompilador.CONST_FLOAT));
 		termo.add(new Integer(CeCompilador.ABREPAR));
@@ -35,7 +182,7 @@ public class First {
 		expressaoAritmetica.add(new Integer(CeCompilador.CONST_INT));
 		expressaoAritmetica.add(new Integer(CeCompilador.CONST_FLOAT));
 		expressaoAritmetica.add(new Integer(CeCompilador.ABREPAR));*/
-		tipo.add(new Integer(CeCompilador.ID));
+		//tipo.add(new Integer(CeCompilador.ID));
 	}
 
 }
--- src/main/java/org/example/cecompiladorspring/controller/CompilerController.java
+++ src/main/java/org/example/cecompiladorspring/controller/CompilerController.java
@@ -11,57 +11,64 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.StringReader;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/compile")
-@CrossOrigin(origins = "*") // Permite requisições de qualquer origem
+@CrossOrigin(origins = "*")
 public class CompilerController {
     @PostMapping
     public ResponseEntity<CompilerResponse> compile(@RequestBody CompileRequest request) {
         try {
-            //CeCompilador parser = new CeCompilador(new StringReader(""));
+            CeCompilador.reset();
+            CeCompilador.ReInit(new StringReader(request.getCode()));
+            CeCompilador.main();
 
-            CeCompilador.ReInit(new StringReader(request.getCode())); // Re-inicializa o parser com o código recebido
+            if (!CeCompilador.getErrors().isEmpty()) {
+                List<ErrorDetails> errorDetailsList = CeCompilador.getErrors().stream()
+                        .filter(error -> error instanceof ParseException)
+                        .map(error -> {
+                            ParseException e = (ParseException) error;
+                            Token tokenErro = e.currentToken;
+                            String simboloEncontrado = tokenErro.next != null ? tokenErro.next.image : "fim de arquivo";
+                            String simboloEsperado = e.expectedTokenSequences != null && e.tokenImage != null
+                                    ? e.tokenImage[e.expectedTokenSequences[0][0]]
+                                    : "Desconhecido";
+                            int linha = tokenErro.next != null ? tokenErro.next.beginLine : -1;
+                            int coluna = tokenErro.next != null ? tokenErro.next.beginColumn : -1;
 
-            CeCompilador.main(); // Executa o parser
+                            return new ErrorDetails(simboloEncontrado, simboloEsperado, linha, coluna);
+                        }).toList();
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new CompilerResponse("Erros sintáticos encontrados:", true, errorDetailsList));
+            }
 
             return ResponseEntity.ok(new CompilerResponse("Compilação bem-sucedida!", false));
-        } catch (ParseException e) {
-            Token tokenErro = e.currentToken;
-            String simboloEncontrado = tokenErro.image;
-            String simboloEsperado = e.expectedTokenSequences != null && e.tokenImage != null
-                    ? e.tokenImage[e.expectedTokenSequences[0][0]]
-                    : "Desconhecido";
-            int linha = tokenErro.beginLine;
-            int coluna = tokenErro.beginColumn;
 
-            ErrorDetails errorDetails = new ErrorDetails(simboloEncontrado, simboloEsperado, linha, coluna);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new CompilerResponse("Erro sintático: " + e.getMessage(), true, errorDetails));
         } catch (TokenMgrError e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new CompilerResponse("❌ Erro léxico: " + e.getMessage(), true));
+                    .body(new CompilerResponse("Erro léxico: " + e.getMessage(), true));
         } catch (Exception e) {
             return ResponseEntity.internalServerError()
-                    .body(new CompilerResponse("⚠️ Erro interno: " + e.getMessage(), true));
+                    .body(new CompilerResponse("Erro interno: " + e.getMessage(), true));
         }
     }
 
     public static class CompilerResponse {
         private String message;
         private boolean error;
-        private ErrorDetails errorDetails;
+        private List<ErrorDetails> errorDetailsList;
 
-        // Construtor, getters e setters
         public CompilerResponse(String message, boolean error) {
             this.message = message;
             this.error = error;
         }
 
-        public CompilerResponse(String message, boolean error, ErrorDetails errorDetails) {
+        public CompilerResponse(String message, boolean error, List<ErrorDetails> errorDetailsList) {
             this.message = message;
             this.error = error;
-            this.errorDetails = errorDetails;
+            this.errorDetailsList = errorDetailsList;
         }
 
         public String getMessage() {
@@ -72,8 +79,8 @@ public class CompilerController {
             return error;
         }
 
-        public ErrorDetails getErrorDetails() {
-            return errorDetails;
+        public List<ErrorDetails> getErrorDetailsList() {
+            return errorDetailsList;
         }
     }
 }
